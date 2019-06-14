a7a98de31b555b2805c6473b3258aa98a75bd2e1
Damien Murphy <damienomurchu@gmail.com>
2019-06-14 21:34:56 +0100

    adds user controller and stubs

---
+++ src/main/java/com/example/springapp/controller/userController.java
@@ -0,0 +1,40 @@
+package com.example.springapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class userController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "/user")
+    public User addUser(@RequestBody User newUser) {
+        // TODO implement
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/friendship")
+    public Friendship addFriendship(@RequestBody Friendship newFriendship) {
+        // TODO implement
+        return null;
+    }
+
+    @RequestMapping("/user/{id}/friends")
+    public ArrayList<User> getFriends(@PathVariable(value = "id") Long userId) {
+        // TODO implement
+        return null;
+    }
+
+    @RequestMapping("/user/{id}/friend/suggestions")
+    public ArrayList<User> getFriendSuggestions(@PathVariable(value = "id") Long userId) {
+        // TODO implement
+        return null;
+    }
+
+    @RequestMapping("/user/{id}/friend/suggestions/bylocation")
+    public ArrayList<User> getFriendSuggestionsByLocation(@PathVariable(value = "id") Long userId) {
+        // TODO implement
+        return null;
+    }
+
+}
