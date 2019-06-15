e0c7a4f40fe04fc9f897b0ff8399aad7c3d551da
Damien Murphy <damienomurchu@gmail.com>
2019-06-15 17:16:09 +0100

    splits out Person and Friendship controller

--- src/main/java/com/example/springapp/controller/PersonController.java
+++ src/main/java/com/example/springapp/controller/PersonController.java
@@ -2,8 +2,6 @@ package com.example.springapp.controller;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-
 @RestController
 public class PersonController {
 
@@ -13,28 +11,5 @@ public class PersonController {
         return null;
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/friendship")
-    public Friendship addFriendship(@RequestBody Friendship newFriendship) {
-        // TODO implement
-        return null;
-    }
-
-    @RequestMapping("/user/{id}/friends")
-    public ArrayList<Person> getFriends(@PathVariable(value = "id") Long userId) {
-        // TODO implement
-        return null;
-    }
-
-    @RequestMapping("/user/{id}/friend/suggestions")
-    public ArrayList<Person> getFriendSuggestions(@PathVariable(value = "id") Long userId) {
-        // TODO implement
-        return null;
-    }
-
-    @RequestMapping("/user/{id}/friend/suggestions/bylocation")
-    public ArrayList<Person> getFriendSuggestionsByLocation(@PathVariable(value = "id") Long userId) {
-        // TODO implement
-        return null;
-    }
 
 }
