f27f867ff32765186daed6002c828bcd873b92aa
Damien Murphy <damienomurchu@gmail.com>
2019-06-15 16:58:08 +0100

    adds PersonRespository

--- src/main/java/com/example/springapp/SpringappApplication.java
+++ src/main/java/com/example/springapp/SpringappApplication.java
@@ -3,8 +3,10 @@ package com.example.springapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.springapp.repositories")
 @EntityScan(basePackages = "com.example.springapp.controller")
 public class SpringappApplication {
 
---
+++ src/main/java/com/example/springapp/repositories/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.example.springapp.repositories;
+
+import com.example.springapp.controller.Person;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Qualifier(value = "personRepository")
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
