0a3e11c14985decdd50fcd6e6d28e8545c6296ca
Damien Murphy <damienomurchu@gmail.com>
2019-06-15 14:34:40 +0100

    fixes entity scan

--- src/main/java/com/example/springapp/SpringappApplication.java
+++ src/main/java/com/example/springapp/SpringappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.example.springapp.model")
+@EntityScan(basePackages = "com.example.springapp.controller")
 public class SpringappApplication {
 
 	public static void main(String[] args) {
